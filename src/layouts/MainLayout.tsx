095d33a00f0bf1ece22720ed8f33cb237b0b3959
Nam Nguyen <nam.nguyen@saritasa.com>
2025-04-12 11:34:00 +0700

    Fixed layout issue

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,9 +3,9 @@ import {FC, ReactNode} from "react";
 
 const MainLayout: FC<{ children?: ReactNode | ReactNode[] }> = ({ children }) => {
     return (
-        <div className="flex min-h-screen bg-gray-300">
+        <div className="flex flex-row min-h-screen bg-gray-300">
             <Sidebar />
-            <main className="flex-1 ml-64 transition-all duration-300">
+            <main className="flex-1 ml-72 transition-all duration-300">
                 { children }
             </main>
         </div>
