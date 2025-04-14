aca180c54a88da2ff97ce9496f6fc0fe6fc0c3e7
thanh ha <thanhhanguyen6103@gmail.com>
2025-04-14 23:21:21 +0700

    change route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout.tsx';
+import LandingPage from "./pages/LandingPage.tsx";
 import CalculationMatrix from './pages/CalculationMatrix.tsx';
 import CriteriaComparisonMatrix from './pages/CriteriaComparisonMatrix.tsx';
-import LandingPage from './components/LandingPage.tsx';
 
 
 const AppRoutes =[
