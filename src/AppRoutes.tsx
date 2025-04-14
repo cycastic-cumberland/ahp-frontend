f1bb8fd3835f2cd0447a8d1803b851630900e2c5
thanh ha <thanhhanguyen6103@gmail.com>
2025-04-14 21:52:07 +0700

    change name in PascalCasePascalCase

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,8 +1,8 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layouts from './layouts/Layouts.tsx';
 import Landing from "./pages/landing.tsx";
-import CalculationMatrix from './pages/calculationMatrix.tsx';
-import CriteriaComparisonMatrix from './pages/criteriaComparisonMatrix.tsx';
+import CalculationMatrix from './pages/CalculationMatrix.tsx';
+import CriteriaComparisonMatrix from './pages/CriteriaComparisonMatrix.tsx';
 
 
 const AppRoutes =[
